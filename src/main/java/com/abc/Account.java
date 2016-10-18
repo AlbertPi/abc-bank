e36b0da3fb59aa71631c0d11d95fc2dfaa3875ea
AlbertPi <albertpi0714@gmail.com>
2016-10-17 21:43:10 -0400

    Update Account.java

--- src/main/java/com/abc/Account.java
+++ src/main/java/com/abc/Account.java
@@ -11,11 +11,21 @@ public class Account {
 
     private final int accountType;
     public List<Transaction> transactions;
+    
+    private double balance;
 
-    public Account(int accountType) {
-        this.accountType = accountType;
-        this.transactions = new ArrayList<Transaction>();
-    }
+    public Account() { // albert pi
+         balance = 0.0;
+       }
+     
+     public Account(int accountType, double balance) {
+          this.accountType = accountType;
+         
+          this.balance = balance; // albert pi
+         
+          this.transactions = new ArrayList<Transaction>();
+      }
+    
 
     public void deposit(double amount) {
         if (amount <= 0) {
@@ -25,6 +35,12 @@ public class Account {
         }
     }
 
+    public void rollback(Account fromAcct,Account toAcct,double amount) {
+               fromAcct.balance = balance + amount;
+               toAcct.balance   = balance-amount;
+       
+    }
+    
 public void withdraw(double amount) {
     if (amount <= 0) {
         throw new IllegalArgumentException("amount must be greater than zero");
@@ -49,6 +65,12 @@ public void withdraw(double amount) {
                     return amount * 0.02;
                 if (amount <= 2000)
                     return 20 + (amount-1000) * 0.05;
+                if((amonut == balance) && past10days)  // albert pi
+                   return balance+(balance* 0.05);
+                else 
+                    return balance+(balance*0.01);
+                                                       // albert pi
+                                                      
                 return 70 + (amount-2000) * 0.1;
             default:
                 return amount * 0.001;
@@ -70,4 +92,37 @@ public void withdraw(double amount) {
         return accountType;
     }
 
-}
+    public void transferTo(Account fromAcct,Account toAcct, double amount) { // albert pi
+        if((fromAcct.balance > amount) && (amount > 0)) {        
+           fromAcct.withdraw(amount);
+           toAcct.deposit(amount);
+           System.out.println("Transfer succesful. Tansfered: $" + amount);
+       } else if(amount <= 0) {
+            rollback(fromAcct,toAcct,amount);
+            transactions.add(new Transaction(amount));
+            System.out.println("Invalid amount to transfe, Transaction cancelled.");
+       } else { 
+           transactions.add(new Transaction(amount));
+           System.out.println("Transfer failed, not enough balance!");
+        }
+    }
+                     
+   public boolean past10days(){
+     Date today = Calendar.getInstance().getTime();
+     Calendar cal = Calendar.getInstance()
+     String current = cal.toSstring():
+     String past = cal.add(Calendar.DATE, -10).toString();
+     if(current.equals(past))
+        return true
+         
+    return false;
+     
+ }      
+  public double Accure() { // albert pi
+      
+      return balance+(balance*0.015);  // rates above are per-annum
+      
+  }
+    
+
+
